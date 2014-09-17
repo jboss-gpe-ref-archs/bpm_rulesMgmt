5086dbb9f7823054423e2200ee0678d44a48fefa
ratwater <jbride2001@yahoo.com>
2014-09-16 21:21:26 -0600

    new ejb-client

---
+++ ejb-client/src/main/java/com/redhat/gpe/GPEExtensionsClient.java
@@ -0,0 +1,32 @@
+package com.redhat.gpe;
+
+import java.util.Hashtable;
+import java.util.logging.Logger;
+
+import javax.naming.InitialContext;
+import javax.naming.Context;
+
+import org.kie.services.remote.ejb.IRemoteRulesMgmtService;
+
+public class GPEExtensionsClient {
+
+    private static final Logger log = Logger.getLogger(GPEExtensionsClient.class.getName());
+    private static final String GPE_EXTENSIONS_REMOTE_INTERFACE="ejb:/business-central/rulesMgmtService!org.kie.services.remote.ejb.IRemoteRulesMgmtService";
+    private static final String DEPLOYMENT_ID = "deploymentId";
+
+    public static void main(String args[])  throws Exception{
+        String deploymentId = System.getProperty(DEPLOYMENT_ID);
+        log.info("main() deploymentId = "+deploymentId);
+
+        Hashtable jndiProps = new Hashtable();
+        //jndiProps.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
+        //jndiProps.put(Context.PROVIDER_URL,"remote://localhost:4447");
+        jndiProps.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+        Context jndiContext = new InitialContext(jndiProps);
+
+        IRemoteRulesMgmtService rMgmtService = (IRemoteRulesMgmtService)jndiContext.lookup(GPE_EXTENSIONS_REMOTE_INTERFACE);
+
+        log.info("main() invoking logRules() with deploymentId = "+deploymentId);
+        rMgmtService.logFacts(deploymentId);
+    }
+}
